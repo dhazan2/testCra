56289cd3cc1c6f44ca09cc1ed328804d3d98732d
dor hazan <dhazan2@gmail.com>
2023-09-30 15:19:43 +0300

    normalizeURL and test

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -4,10 +4,31 @@ import { normalizeURL } from './crawl';
 describe('normalizeURL', () => {
     beforeEach(() => {});
 
-    it('should get the current environment', () => {
-        const input: string = '';
+    it('normalizeURL strip https protocl', () => {
+        const input: string = 'https://blog.boot.dev/path';
         const actual: string = normalizeURL(input);
-        const expected: string = '';
+        const expected: string = 'blog.boot.dev/path';
+        expect(actual).toEqual(expected);
+    });
+
+    it('normalizeURL strip http protocl', () => {
+        const input: string = 'http://blog.boot.dev/path';
+        const actual: string = normalizeURL(input);
+        const expected: string = 'blog.boot.dev/path';
+        expect(actual).toEqual(expected);
+    });
+
+    it('normalizeURL strip trailing slash', () => {
+        const input: string = 'https://blog.boot.dev/path/';
+        const actual: string = normalizeURL(input);
+        const expected: string = 'blog.boot.dev/path';
+        expect(actual).toEqual(expected);
+    });
+
+    it('normalizeURL capitals', () => {
+        const input: string = 'https://BLOG.boot.dev/path/';
+        const actual: string = normalizeURL(input);
+        const expected: string = 'blog.boot.dev/path';
         expect(actual).toEqual(expected);
     });
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,5 +1,12 @@
+import { URL } from 'url';
+
 const normalizeURL = (url: string): string => {
-    return url;
+    const urlObj = new URL(url);
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
 };
 
 export { normalizeURL };
